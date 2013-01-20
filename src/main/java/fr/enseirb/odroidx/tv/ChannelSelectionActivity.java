aafe40de51ad24fa0e1e51c1c4a43f8707eb8755
Fabien Fleurey <fabien@fleurey.org>
2013-01-20 14:45:25 +0100

    fix warnings

--- src/main/java/fr/enseirb/odroidx/tv/ChannelSelectionActivity.java
+++ src/main/java/fr/enseirb/odroidx/tv/ChannelSelectionActivity.java
@@ -22,7 +22,6 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.Window;
 import android.widget.ImageView;
 import android.widget.ListView;
 
